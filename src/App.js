b1a9009d1da981e52a09c7168fd48aa8d2a1533e
AZHAR <Azhar89143@gmail.com>
2021-09-28 23:35:18 -0700

    logo fixed

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Componant/Navbar";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+    </div>
+    
+  );
+  
+ } 
+
+
+  
+
+
+export default App;
