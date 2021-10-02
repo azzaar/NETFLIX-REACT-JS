b7b9f81107746b6e68a037a687806276f4b46750
AZHAR <Azhar89143@gmail.com>
2021-10-03 05:10:51 +0530

    row page completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Componant nav/Navbar";
-import {action,orginals} from './urls.js';
+import {action,comedy,horror,orginals,romance,documentari} from './urls.js';
 import './App.css';
 import Banner from "./Componant ban/Banner";
 import Rowpost from "./componat row/Rowpost";
@@ -13,6 +13,14 @@ function App() {
      <Banner/>
      <Rowpost url={orginals} title="Netflix Orginals"/>
      <Rowpost url={action} title="Actions" isSmall/>
+     <Rowpost url={horror} title="Horror Movies" isSmall/>
+     <Rowpost url={romance} title="Romantic" isSmall/>
+     <Rowpost url={comedy} title="Comedy Movies" isSmall/>
+     <Rowpost url={documentari} title="Documentaries" isSmall/>
+
+
+
+
     </div>
     
   );
--- src/urls.js
+++ src/urls.js
@@ -2,3 +2,10 @@ import {API_KEY} from './constents/Constant';
 
 export const orginals =`discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action= `discover/movie?api_key=${API_KEY}&with_genres=28`
+export const horror= `discover/movie?api_key=${API_KEY}&with_genres=27`
+export const romance= `discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const comedy= `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const documentari= `discover/movie?api_key=${API_KEY}&with_genres=99`
+
+
+
